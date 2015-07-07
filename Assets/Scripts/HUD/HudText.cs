46e19e41f97656b308218737635e7603b373a3ce
Jéssica Caroline <jc.jooy@gmail.com>
2015-07-07 20:01:15 -0300

    Documentation of HudText

--- Assets/Scripts/HUD/HudText.cs
+++ Assets/Scripts/HUD/HudText.cs
@@ -2,6 +2,10 @@
 using System.Collections;
 using UnityEngine.UI;
 
+//! Handles the text.
+/*!
+ * Contains three methods that handles the text (instance, set and erase). 
+ */
 
 public class HudText : MonoBehaviour 
 {
@@ -9,6 +13,7 @@ public class HudText : MonoBehaviour
 	public static HudText instance;
 
 	// Use this for initialization
+	//! Returns the component of type TextObject.
 	void Start () 
 	{
 		instance = this;
@@ -16,15 +21,16 @@ public class HudText : MonoBehaviour
 		textObject = instance.gameObject.GetComponent<Text> ();
 	}
 
+	//! Set the input text.
 	public static void SetText(string text)
 	{
 		if(textObject != null)
 			textObject.text = text;
 	}
 
+	//! Erase the text.
 	public static void EraseText()
 	{
-
 		if(textObject != null)
 			textObject.text = "";
 	}

2e92e00b260b17ab6ce664cfeb4d05fdf9712d7e
Jéssica Caroline <jc.jooy@gmail.com>
2015-07-07 20:39:44 -0300

    Documentation of HUDController

--- Assets/Scripts/HUD/HUDController.cs
+++ Assets/Scripts/HUD/HUDController.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//! Control of the player interaction interface.
+/*!
+ * Contains three methods that enable or desable the components of gameObject
+ * and change the HUDCamera.
+ */
+
 public class HUDController : MonoBehaviour {
 
+	//!Set the local state of the gameObject.
+	/*! Making a gameObject inactive will desable every component. */
     public void disableHUD()
     {
         this.gameObject.SetActive(false);
     }
-
+	//!Set the local state of the gameObject.
     public void enableHUD()
     {
         this.gameObject.SetActive(true);
     }
+	//! Change the camera when the player interacts with an object.
+	/*! Used as the Camera that events will be sent through for a world space (canvas: element that can used for screen rendering). */
     public void changeHUDCamera(Camera newCamera)
     {
         GetComponent<Canvas>().worldCamera = newCamera;

fffad2f0d02e6998ebc3f454362b124e95e3a171
thamenato <thamenato@gmail.com>
2015-07-08 16:50:16 -0300

    GameController documentation and TODOs

--- Assets/Scripts/InGame/GameController.cs
+++ Assets/Scripts/InGame/GameController.cs
@@ -2,15 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 
+//! Game Controller
+/*! Script that controls the current state of the game - therefore, it is where the 'State Machine' changes. 
+ */
 public class GameController : MonoBehaviour {
 
 	private CharacterController player;
 
-	public List<GameStateBase> gameStates = new List<GameStateBase>(); 
+	public List<GameStateBase> gameStates = new List<GameStateBase>();  /*!< List with all game 'states' of the game. */
 	private GameStateBase currentGameState;
 
 	public static GameController instance;
 
+    //TODO: Remover variaveis de 'Suporte', renomear Backer para nome correto: Beaker
 	//variaveis para suprir a falta de inventario.
 	public bool haveReagentNaCl;
 	public int totalBackers;
@@ -19,7 +23,10 @@ public class GameController : MonoBehaviour {
 
 	//variaveis para suprir a falta de inventario.
 
-	// Use this for initialization
+    //TODO: No Start esta puxando um Glassware do prefab e jogando para selectedGlassware.
+    /*Comentario: Esse glassware teoricamente eh apenas para suprir a falta do inventario, nao sei se estar dentro do GameController eh a melhor
+     * solucao, talvez uma comunicacao entre o script do 'state' (ex.: Balanca) com o inventario e objeto selecionado atualmente seja mais facil/intuitivo.
+     */
 	void Start () {
 
 		selectedGlassWare = prefabBacker.GetComponent<Glassware>();
@@ -40,16 +47,21 @@ public class GameController : MonoBehaviour {
 		}
 	}
 
+    //! Change current state to another one.
+    /*! Using the index from list of game states, changes current state to another one. */
 	public void ChangeState(int indexState){
 
 		GameStateBase selectState = gameStates[indexState];
 
+        // TODO: Esse StopRun() e StartRun() eh meio nebuloso, verificar a real necessidade disso.
 		currentGameState.StopRun();
 		currentGameState = selectState;
 		currentGameState.StartRun();
 		
 	}
 
+    //! Go back to Default State.
+    /*! Changes current state to default state: defined as index 0 from list of game states. */
 	public void GoToDefaultState(){
 		ChangeState(0);
 	}

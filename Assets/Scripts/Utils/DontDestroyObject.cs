549bcfd2d9db2e381e812da6a149b9fb92d3c20c
Jéssica Caroline <jc.jooy@gmail.com>
2015-07-08 01:41:27 -0300

    Documentation of DontDestroyObject

--- Assets/Scripts/Utils/DontDestroyObject.cs
+++ Assets/Scripts/Utils/DontDestroyObject.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//! Preserve an object during level loading.
+/*!
+ * Contains one method that makes the object target
+ * not be destroyed automatically when loading a new scene.
+ */
+
 public class DontDestroyObject : MonoBehaviour 
 {
-
+	/*! 
+	 * Awake is used to initialize any variables or game state before the game starts.
+	 * Awake is called only once during the lifetime of the script instance.*/
 	void Awake () {
-
 		DontDestroyOnLoad (this.gameObject);
-	
 	}
-	
 }

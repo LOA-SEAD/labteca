86b2086b55c1e68763a219bbde0788da65e6011e
Jéssica Caroline <jc.jooy@gmail.com>
2015-07-08 15:09:52 -0300

    Documentation of FadeScript

--- Assets/Scripts/Utils/FadeScript.cs
+++ Assets/Scripts/Utils/FadeScript.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//! Makes the fade used in a animation transition.
+/*!
+ * Contains two methods that makes the fade used in a animation transition (make and show).
+ */
+// TODO: Testar para saber exatamente como funciona.
 public class FadeScript : MonoBehaviour {
 
 	public static FadeScript instance;
 	public Animator fadeAnim;
 
+	//!  The script instance is being loaded (before the game starts).
+	/*! Get any object of type FadeScript and if more than 1 is returned - there are two on scene, so delete this one. */
 	void Awake () {
 		instance = this;
            
@@ -15,10 +22,10 @@ public class FadeScript : MonoBehaviour {
         // if more than 1 is returned - there are two on scene, so delete this one
 		if(duplicate.Length > 1)
 			Destroy(gameObject);
-	
 	}
 
-    public void ShowFade(){
+	//! Sets a trigger parameter to active.
+	public void ShowFade(){
 		instance.fadeAnim.SetTrigger("fade");
 	}
 }

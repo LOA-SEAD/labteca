9c54f0a8d74e95f596f4a1bf0bbc7ba764126b2b
Jéssica Caroline <jc.jooy@gmail.com>
2015-07-08 17:40:03 -0300

    Documentation of glassware

--- Assets/Scripts/Components/Glassware.cs
+++ Assets/Scripts/Components/Glassware.cs
@@ -3,6 +3,11 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
+//! Player interaction with the Glassware.
+/*! Contains seven methods that makes the interaction with the glassware
+* add and remove liquids and solids.
+*/
+
 public class Glassware : MonoBehaviour 
 {
 	public float volume;
@@ -19,13 +24,13 @@ public class Glassware : MonoBehaviour
 
 	public List<ReagentsInGlass> reagents = new List<ReagentsInGlass>();
 
-	[System.Serializable]
+	[System.Serializable] /*!< Lets you embed a class with sub properties in the inspector. */
 	public class ReagentsInGlass{
 		public string reagentName;
 		public float massReagent;
 	}
 
-
+	//!  Is called when the script instance is being loaded.
 	void Awake()
 	{
 		solid.SetActive(false);
@@ -33,24 +38,26 @@ public class Glassware : MonoBehaviour
 	}
 
 	// Use this for initialization
+	//! Sets a mass to rigidbody
 	void Start () 
 	{
 		this.rigidbody.mass = mass;
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	/*void Update () 
 	{
 	
-	}
+	}*/
 	
-
+	//! Add the solid
 	public void AddSolid(float massSolid, string reagent){
 		if(liquid.activeSelf == false)
 			solid.SetActive(true);
 		GetComponent<Rigidbody>().mass += massSolid;
 	}
 
+	//! Remove the solid
 	public void RemoveSolid(float massSolid){
 		GetComponent<Rigidbody>().mass -= massSolid;
 		if(GetComponent<Rigidbody>().mass < mass){
@@ -59,6 +66,8 @@ public class Glassware : MonoBehaviour
 		}
 	}
 
+	//! Add the liquid
+	/*! Checks the current volume (higher, lower or equal) and add the liquid. */
 	public void AddLiquid(float massLiquid, float volumeLiquid){
 
 		if(currentVolumeUsed < volume){
@@ -83,16 +92,15 @@ public class Glassware : MonoBehaviour
 
             Debug.Log("Recipiente esta cheio");
             //AlertDialogBehaviour.ShowAlert("Recipente esta cheio");
-
 		}
-
-
 	}
 
 	public void AddLiquid(float volumeLiquid){
 		AddLiquid (1, volumeLiquid);
 	}
-	
+	//TODO: Dois metodos AddLiquid e RemoveLiquid?!
+
+	//! Remove the liquid.
 	public void RemoveLiquid(float massLiquid, float volumeLiquid){
 
 		GetComponent<Rigidbody>().mass -= massLiquid*volumeLiquid;
@@ -112,14 +120,13 @@ public class Glassware : MonoBehaviour
 		RemoveLiquid (1, volumeLiquid);
 	}
 
+	//! Sets the glassware state.
 	public void SetStateInUse(GameStateBase state){
 		stateInUse = state;
-
 	}
 
+	//!Message when the player clicks in glass.
 	public void CLickInGlass(){
 		stateInUse.SendMessage ("ClickGlass", gameObject, SendMessageOptions.DontRequireReceiver);
 	}
-
-
 }

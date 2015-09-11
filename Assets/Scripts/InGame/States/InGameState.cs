8e63881480827d90362739fd35dfd4c5c3a4a85a
ThiagoMorano <thiago.morano159@gmail.com>
2015-09-11 16:17:36 -0300

    Testing Inventory in InGameState
    Testing how to show the inventory in the InGameState

--- Assets/Scripts/InGame/States/InGameState.cs
+++ Assets/Scripts/InGame/States/InGameState.cs
@@ -6,9 +6,22 @@ using System.Collections;
 public class InGameState : GameStateBase {
 
 	public GameObject player; /*< GameObject of Player. */
+	public Canvas inventoryCanvas;
 
 	void Update(){
 		base.Update();	// base is the GameStateBase
+		//////////////
+		if (Input.GetKeyDown (KeyCode.I)) {
+			//GameObject.Find("canvas_inventory 1").SetActive(!GameObject.Find("canvas_inventory 1").activeSelf);
+			player.GetComponent<MouseLook>().enabled = !player.GetComponent<MouseLook>().enabled;
+			player.GetComponent<CharacterMotor>().enabled = !player.GetComponent<CharacterMotor>().enabled;
+			player.GetComponent<FPSInputController>().enabled = !player.GetComponent<FPSInputController>().enabled;
+			inventoryCanvas.enabled = !inventoryCanvas.enabled;
+		}
+		/*if (Input.GetKey (KeyCode.I)) {
+			Debug.LogError("Exiting InventoryState");
+			gameController.ChangeState (4);
+		}*/
 	}
 
 	protected override void UpdateState ()

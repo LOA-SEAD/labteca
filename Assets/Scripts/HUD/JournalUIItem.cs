ab3433e6219a10f3a99854fd2742e172af9fb235
Leonardo Tavares <628174@comp.ufscar.br>
2015-09-29 16:09:36 -0300

    JournalUIItem Created
    -DemoLabDev.unity:
    --Prefab of JournalUIItem created and added to Journal

---
+++ Assets/Scripts/HUD/JournalUIItem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JournalUIItem : MonoBehaviour {
+	
+	public bool isDone;
+	public JournalUIItem[] prerequisites;
+
+	public void checkItem(){
+		if (!isDone && checkPrerequisites ()){
+			isDone=true;
+		}
+	}
+	
+	private bool checkPrerequisites(){
+		bool allDone=true;
+		for (int i=0; i < prerequisites.Length&&allDone; i++) {
+			allDone = prerequisites[i].isDone;
+		}
+		return allDone;
+	}
+}

547449c9ed2a63127e4d8e9a713439d91037bc37
Leonardo Tavares <628174@comp.ufscar.br>
2015-10-02 17:49:34 -0300

    DemoLabDev fix & JournalUIItem changes

--- Assets/Scripts/HUD/JournalUIItem.cs
+++ Assets/Scripts/HUD/JournalUIItem.cs
@@ -1,22 +1,36 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class JournalUIItem : MonoBehaviour {
-	
-	public bool isDone;
+	public bool isDone,prerequisitesDone;
 	public JournalUIItem[] prerequisites;
 
+	public void Start(){
+		checkPrerequisites ();
+	}
+
 	public void checkItem(){
-		if (!isDone && checkPrerequisites ()){
+		if (!isDone && prerequisitesDone){
 			isDone=true;
+			GameObject[] journalItems = GameObject.FindGameObjectsWithTag("JournalUIItem");
+			for(int i = 0;i<journalItems.Length;i++){
+				journalItems[i].GetComponent<JournalUIItem>().checkPrerequisites();
+			}
 		}
 	}
 	
-	private bool checkPrerequisites(){
+	public void checkPrerequisites(){
 		bool allDone=true;
 		for (int i=0; i < prerequisites.Length&&allDone; i++) {
 			allDone = prerequisites[i].isDone;
 		}
-		return allDone;
+
+		if (allDone) {
+			prerequisitesDone=true;
+			this.GetComponent<Image> ().color = new Color (this.GetComponent<Image> ().color.r, this.GetComponent<Image> ().color.g, this.GetComponent<Image> ().color.b, 100 / 255f);
+		}
+		else
+			this.GetComponent<Image> ().color = new Color (this.GetComponent<Image> ().color.r, this.GetComponent<Image> ().color.g, this.GetComponent<Image> ().color.b, 0f);
 	}
 }

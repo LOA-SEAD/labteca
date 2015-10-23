3eb8b6c3c4834e92e931145e661b197861ec9953
Leonardo Tavares <leonardotavares08@hotmail.com>
2015-10-23 16:37:22 -0200

    InventoryItem created

---
+++ Assets/inventoryItem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class inventoryItem : MonoBehaviour {
+
+	public Image itemIcon;
+	public Text nameText,actionText;
+	//public GameObject 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

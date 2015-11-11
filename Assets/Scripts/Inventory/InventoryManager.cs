eb2df5ef567c1ea91baae8d8241c7b9971950a16
ThiagoMorano <thiago.morano159@gmail.com>
2015-11-11 20:28:26 -0200

    Add Workbench
    Added the WorkBench script as it should be.
    
    - In this commit, the workbench is working properly only in the FumeHood
    state and without the inventory interactions.

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -160,7 +160,7 @@ public class InventoryManager : MonoBehaviour {
 		GameObject glasswareInScene = Instantiate(bequerPrefab) as GameObject;// = Instantiate the glassware saved in the informations
 
 		//This here will be done using the PutGlassInTable() method from the WorkBench.
-		glasswareInScene.transform.position = gameController.gameStates [gameController.currentStateIndex].GetComponent<FumeHoodState> ().positionGlass1.transform.position;
+//		glasswareInScene.transform.position = gameController.gameStates [gameController.currentStateIndex].GetComponent<FumeHoodState> ().positionGlass1.transform.position;
 
 	}
 

5f6769219c484dcc9a05a9a702541fb2a63694e2
Leonardo Tavares <628174@comp.ufscar.br>
2016-03-23 19:02:30 -0300

    Add image description

--- Assets/Scripts/Scenario/AccessEquipmentBehaviour.cs
+++ Assets/Scripts/Scenario/AccessEquipmentBehaviour.cs
@@ -1,14 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 //! Allows any Equipment to be interactable.
+
+
+
 public class AccessEquipmentBehaviour : InteractObjectBase {
 
 	public GameStateBase targetEquipment;      /*!< GameStateBase for equipment. */
 	private bool callInteract;
     public float delay = 0.5f;                  /*!< Float delay time. */
 	private float currentDelay;
-	public string informationText;
+	public Canvas descriptionCanvas;
+
+	void Start(){
+		if (descriptionCanvas != null) {
+			Debug.Log(descriptionCanvas.name);
+			descriptionCanvas.enabled = false;
+		}
+	}
 
 	void Update () {
 		if(callInteract){

c9860425f799a8cfa1a7fea96a8b54d614f6b6d3
Leonardo Tavares <628174@comp.ufscar.br>
2016-07-01 16:26:13 -0300

    Added phmeter (not implemented)

---
+++ Assets/Scripts/Machinary/PHMeterController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PHMeterController : EquipmentControllerBase {
+	public override void AddObjectInEquipament(GameObject objectToAdd){
+
+	}
+	
+	//! Remove a GameObject from being measured on Scale.
+	public override void RemoveObjectInEquipament(GameObject objectToRemove){
+
+	}
+}

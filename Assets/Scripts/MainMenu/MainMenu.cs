eb03bd81c14bd71a4f4fbf62f04db02fd2205029
ThiagoMorano <thiago.morano159@gmail.com>
2016-09-06 17:58:06 -0300

    Add MainMenu base

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour {
+
+	public GameObject mainButtonsPanel;
+
+
+	public GameObject loadPanel;
+
+
+
+	public GameObject optionsPanel;
+	public Slider musicSlider;
+	public Text musicText;
+	public Slider effectsSlider;
+	public Text effectsText;
+
+	private const string musicVolume = "musicVolume";
+	private const string effectsVolume = "effectsVolume"; //TODO: VERIFY STRINGS OF VARIABLES
+
+
+
+	// Use this for initialization
+	void Start () {
+		ReturnToMainMenu ();
+
+		if (!PlayerPrefs.HasKey (musicVolume)) {
+			PlayerPrefs.SetFloat (musicVolume, 1.0f);
+		}
+		if (!PlayerPrefs.HasKey (effectsVolume)) {
+			PlayerPrefs.SetFloat (effectsVolume, 1.0f);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Close all panels when called
+	public void CloseAllPanels() {
+		mainButtonsPanel.SetActive (false);
+		optionsPanel.SetActive (false);
+		//loadPanel.SetActive (false);
+	}
+
+	//Method used to go back to the main menu
+	public void ReturnToMainMenu() {
+		CloseAllPanels ();
+		mainButtonsPanel.SetActive (true);
+	}
+
+	//-----------------
+	//BUTTONS PANEL
+
+	//Called when the Play Button is pressed
+	public void PlayButton() {
+
+	}
+
+	//Called when the Load button is pressed
+	public void LoadButton() {
+
+	}
+	
+	//Called when the Options button is pressed
+	public void OptionsButton() {
+		CloseAllPanels ();
+		OnStartOptionsPanel ();
+	}
+
+	//Called when the Credits button is pressed
+	public void CreditsButton() {
+	
+	}
+
+	//Called when the Exit button is pressed
+	public void ExitButton() {
+
+	}	
+
+	//-------------------------
+	//OPTIONS PANEL
+	
+	//Starts the OptionsPanel
+	public void OnStartOptionsPanel () {
+		optionsPanel.SetActive (true);
+
+		musicSlider.value = PlayerPrefs.GetFloat (musicVolume) * 100.0f;
+		effectsSlider.value = PlayerPrefs.GetFloat (effectsVolume) * 100.0f;
+	}
+
+	//Called while the music volume slider is being dragged
+	public void UpdateMusicVolume() {
+		musicText.text = musicSlider.value.ToString ();
+	}
+	//Called while the effects volume slider is being dragged
+	public void UpdateEffectsVolume() {
+		effectsText.text = effectsSlider.value.ToString ();
+	}
+
+	//Called on EndDrag of MusicVolume slider
+	public void EndDragMusicVolume() {
+		PlayerPrefs.SetFloat (musicVolume, musicSlider.value / 100.0f);
+	}
+	//Called on EndDrag of EffectsVolume slider
+	public void EndDragEffectsVolume() {
+		PlayerPrefs.SetFloat (effectsVolume, effectsSlider.value / 100.0f);
+	}
+
+	//-------------------------
+
+
+}

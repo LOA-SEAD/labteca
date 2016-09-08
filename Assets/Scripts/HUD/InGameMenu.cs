b21535c5c0c18d59eddfc72baf813d3c713dba97
Leonardo <leonardotavares08@hotmail.com>
2016-09-07 21:10:30 -0300

    InGameMenu WIP

---
+++ Assets/Scripts/HUD/InGameMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGameMenu : MonoBehaviour {
+	public void Pause(){
+		Time.timeScale = 0f;
+	}
+
+	public void UnPause(){
+		//Time.timeScale
+	}
+}

74310138fb96b2a2f4ac0d20e727f01a16451901
ThiagoMorano <thiago.morano159@gmail.com>
2017-09-20 17:51:04 -0300

    Start Tablet refactoring.

---
+++ Assets/Scripts/Tablet/NewTablet/TabletController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum TabletSubstate {
+	HomeState = 0,
+	ExperimentState = 1,
+	JournalState = 2,
+	NotesState = 3,
+	HandbookState = 4,
+	ReagentInfoState = 5,
+	GraphicsState = 6
+}
+
+public class TabletController : MonoBehaviour {
+
+	public Hashtable tabletMap = new Hashtable();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

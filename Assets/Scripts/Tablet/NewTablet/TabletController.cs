0b048cc00837ef917dd19198bcc2287d933c17ac
ThiagoMorano <thiago.morano159@gmail.com>
2017-09-21 13:35:40 -0300

    New Tablet State Machine added.

--- Assets/Scripts/Tablet/NewTablet/TabletController.cs
+++ Assets/Scripts/Tablet/NewTablet/TabletController.cs
@@ -6,24 +6,74 @@ using UnityEngine;
 public enum TabletSubstate {
 	HomeState = 0,
 	ExperimentState = 1,
-	JournalState = 2,
-	NotesState = 3,
-	HandbookState = 4,
-	ReagentInfoState = 5,
-	GraphicsState = 6
+	NotesState = 2,
+	HandbookState = 3,
+	GraphicsState = 4,
+	JournalState = 5,
+	ReagentInfoState = 6
 }
 
 public class TabletController : MonoBehaviour {
-
-	public Hashtable tabletMap = new Hashtable();
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
+	public GameObject Tablet;
+	public GameObject HomeState;
+	public GameObject ExperimentState;
+	public GameObject JournalState;
+	public GameObject NotesState;
+	public GameObject HandbookState;
+	public GameObject ReagentInfoState;
+	public GameObject GraphicsState;
+
+	public void ChangeTabletState(int state) {
+		switch ((TabletSubstate)state) {
+		case TabletSubstate.HomeState:
+			CloseAllState ();
+			HomeState.SetActive(true);
+			break;
 		
+		case TabletSubstate.ExperimentState:
+			CloseAllState ();
+			ExperimentState.SetActive(true);
+			break;
+		
+		case TabletSubstate.JournalState:
+			CloseAllState ();
+			JournalState.SetActive(true);
+			break;
+		
+		case TabletSubstate.NotesState:
+			CloseAllState ();
+			NotesState.SetActive(true);
+			break;
+		
+		case TabletSubstate.ReagentInfoState:
+			CloseAllState ();
+			ReagentInfoState.SetActive(true);
+			break;
+		
+		case TabletSubstate.GraphicsState:
+			CloseAllState ();
+			GraphicsState.SetActive(true);
+			break;
+		}
+	} 
+
+
+	protected void OpenTablet() {
+		Tablet.SetActive(true);
+	}
+	protected void CloseTablet() {
+		Tablet.SetActive(false);	
+	}
+
+
+	private void CloseAllState() {
+		HomeState.SetActive (false);
+		ExperimentState.SetActive (false);
+		/*JournalState.SetActive (false);
+		NotesState.SetActive (false);
+		HandbookState.SetActive (false);
+		ReagentInfoState.SetActive (false);
+		GraphicsState.SetActive (false);*/
 	}
 }
